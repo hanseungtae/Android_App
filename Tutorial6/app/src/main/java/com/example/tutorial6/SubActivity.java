458e10903b09d6f10a04f45a2f5de68ce1a9a06e
HanSeungTae <sg920456@naver.com>
2022-01-11 22:22:05 +0900

    액티비티 화면 전환

---
+++ Tutorial6/app/src/main/java/com/example/tutorial6/SubActivity.java
@@ -0,0 +1,22 @@
+package com.example.tutorial6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_sub);
+        TextView nameText = (TextView) findViewById(R.id.nameText);
+        Intent intent = getIntent();
+        nameText.setText(intent.getStringExtra("nameText").toString());
+
+    }
+}
