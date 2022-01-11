bfeb663478be2d6ae5d9e7f6c93325d58e89758f
HanSeungTae <sg920456@naver.com>
2022-01-11 23:26:15 +0900

    유튜브 API 연동

---
+++ Tutorial7/app/src/main/java/com/example/tutorial7/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.tutorial7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    YouTubePlayerView youTubeView;
+    Button button;
+    YouTubePlayer.OnInitializedListener listener;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        button = (Button) findViewById(R.id.youtubeButton);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+        listener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("f7_ORwhSx2E");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                youTubeView.initialize("AIzaSyDOHqws19GKM2NQWCXBYM0QGnVE0JnOfdo", listener);
+            }
+        });
+    }
+}
